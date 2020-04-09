6f452347d2cafdeadac1ae30ec11fcfd4c304bbf
vitonittwo <vorslavv33@inbox.lv>
2020-04-09 19:52:38 +0300

    was supplemented Stdin and Stdout

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,35 @@
 package com.company;
 
+
+import java.util.Scanner;
+
 public class Main {
 
+
+
     public static void main(String[] args) {
-	System.out.println("Hello, World.");
+        System.out.println("Hello, World.");
         System.out.println("Hello, Java.");
+
+
+            Scanner scan= new Scanner(System.in);
+            // String myString = scan.next();
+            // int myInt = scanner.nextInt();
+           // scanner.close();
+
+            // System.out.println("myString is: " + myString);
+            // System.out.println("myInt is: " + myInt);
+
+           int a = scan.nextInt();
+           int b = scan.nextInt();
+            int c = scan.nextInt();
+
+
+           System.out.println(a);
+           System.out.println(b);
+           System.out.println(c);
+
+
+
     }
 }
