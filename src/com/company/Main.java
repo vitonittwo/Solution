10ee71e2f3a8fe4d7b4881a79b33e143db3f9a98
vitonittwo <vorslavv33@inbox.lv>
2020-04-10 15:01:43 +0300

    input interface changed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,14 @@
 package com.company;
 
-
 import java.util.Scanner;
 
 public class Main {
-
-
 
     public static void main(String[] args) {
         System.out.println("Hello, World.");
         System.out.println("Hello, Java.");
 
-
-            Scanner scan= new Scanner(System.in);
+            Scanner scan = new Scanner(System.in);
             // String myString = scan.next();
             // int myInt = scanner.nextInt();
            // scanner.close();
@@ -20,10 +16,17 @@ public class Main {
             // System.out.println("myString is: " + myString);
             // System.out.println("myInt is: " + myInt);
 
-           int a = scan.nextInt();
-           int b = scan.nextInt();
-            int c = scan.nextInt();
+       int a,b,c;
+      System.out.print("Input a = ");
+         a = scan.nextInt();
+      // System.out.println(a);
+      System.out.print("Input b = ");
+         b = scan.nextInt();
+       // System.out.println(b);
 
+       System.out.print("Input c = ");
+         c = scan.nextInt();
+       // System.out.println(c);
 
            System.out.println(a);
            System.out.println(b);
